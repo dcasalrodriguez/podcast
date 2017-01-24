168119caed86a5169334030ab0e139dd4b97d2f0
Diego Casal Rodriguez <dcasalrodriguez@localhost>
2017-01-24 23:15:09 +0100

    Cuarto commit, filtrado correcto de podcasts

--- js/podcast.js
+++ js/podcast.js
@@ -4,6 +4,10 @@
  */
 jQuery(function() {
 	podcast.getPodcasts();
+	
+	jQuery('#filter').on('keyup', function() {
+		podcast.filterPodcasts(jQuery(this).val());
+	});
 });
 
 var podcast = new function() {
@@ -14,7 +18,7 @@ var podcast = new function() {
 		
 		if (isPodcastsLocal) {
 			var datos = podcast.getPodcastsLocal();
-			podcast.drawResults(datos);
+			podcast.drawResults(datos.feed.entry);
 			util.loaderHide();
 		} else {
 			jQuery.ajax({
@@ -23,7 +27,7 @@ var podcast = new function() {
 				dataType: 'json',
 				success: function(datos) {
 					podcast.setLocalData(datos);
-					podcast.drawResults(datos);
+					podcast.drawResults(datos.feed.entry);
 					util.loaderHide();
 				},
 				error: function() {
@@ -36,35 +40,73 @@ var podcast = new function() {
 	
 	// Pinta los resultados
 	this.drawResults = function(datos) {
-		var source   = $("#podcast-result-template").html();
+		var source   = jQuery("#podcast-result-template").html();
 		var template = Handlebars.compile(source);
 		
 		var obj = {
-			entries : datos.feed.entry
+			entries : datos
 		};
 		
-		console.log(obj);
+		var contador = datos.length;
 		
-		$('#podcasts-list').html(template(obj));
+		jQuery('#podcasts-list').html(template(obj));
+		jQuery('#contador-podcasts').html(contador);
+
+		util.loaderHide();
 	};
 	
 	// Filtra los podcasts
-	this.filterPodcasts = function() {
-		
+	this.filterPodcasts = function(textFilter) {
+		util.loaderShow();
+		var datos = podcast.getPodcastsLocal();
+		if ('' != textFilter) {
+			textFilter = textFilter.toUpperCase();
+			datos = $(datos.feed.entry).filter(function (i,n){
+				var title = n.title.label.toUpperCase();
+				var artist = n['im:artist'].label.toUpperCase();
+				
+				return (-1 < title.indexOf(textFilter) || -1 < artist.indexOf(textFilter))
+			});
+		} else {
+			datos = datos.feed.entry;
+		}
+		podcast.drawResults(datos);
 	};
 	
 	// Obtiene los podcast en local
 	this.getPodcastsLocal = function() {
-		
+		var datos = localStorage.getItem("podcasts");
+		return JSON.parse(datos).datos;
 	};
 	
 	// Identifica si los podcast se han de recuperar de local o hay que pedirlos
 	this.isPodcastsLocal = function() {
+		var hayLocal = false;
+		var datos = localStorage.getItem("podcasts");
 		
+		if (null != datos) {
+			datos = JSON.parse(datos);
+			var fechaDatos = datos.fecha;
+			
+			if (fechaDatos == util.getStringDateNow()) {
+				hayLocal = true;
+			}
+		}
+		
+		return hayLocal;
 	};
 	
 	// Anade los datos en cliente
 	this.setLocalData = function(datos) {
+		var fechaHoy = util.getStringDateNow();
 		
+		var podcasts = {
+				fecha : fechaHoy
+				,datos : datos
+		}
+		
+		podcasts = JSON.stringify(podcasts);
+		
+		localStorage.setItem("podcasts", podcasts);
 	};
 };
--- js/utils.js
+++ js/utils.js
@@ -2,6 +2,17 @@
  * utils.js Contiene funciones de utilidad
  */
 var util = new function() {
+	this.getStringDateNow = function() {
+		var hoy = new Date();
+		var dia = hoy.getDate();
+		if (1 == dia.length) {dia += '0';}
+		var mes = hoy.getMonth() + 1;
+		if (1 == mes.length) {mes += '0';}
+		var anio = hoy.getFullYear();
+		
+		return dia + '/' + mes + '/' + anio;
+	};
+	
 	this.loaderHide = function() {
 		setTimeout(function() {
 			jQuery('.spinner').fadeOut(1000, function() {
@@ -12,6 +23,8 @@ var util = new function() {
 
 	this.loaderShow = function() {
 		var html = '<div class="spinner pull-right"><div class="rect1"></div><div class="rect2"></div><div class="rect3"></div><div class="rect4"></div><div class="rect5"></div></div>';
-		jQuery('.col-nav').append(html);
+		if (0 == jQuery('.spinner').length) {
+			jQuery('.col-nav').append(html);
+		}
 	};
 };
